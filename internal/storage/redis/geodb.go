cf8c46e7880e561ecbd7523773cfe43376020418
Justin-Kwan <justinpkwan@outlook.com>
2020-06-03 22:30:49 -0400

    updated geo db docs

--- internal/storage/redis/geodb.go
+++ internal/storage/redis/geodb.go
@@ -11,6 +11,9 @@ type GeoDB struct {
 	index string
 }
 
+// Returns a new geo database access struct given a redis connection
+// pool and an index name to store all longitude, latitude pairs
+// within the key values store.
 func NewGeoDB(pool *redis.Pool, index string) *GeoDB {
 	return &GeoDB{
 		pool:  pool,
@@ -18,6 +21,9 @@ func NewGeoDB(pool *redis.Pool, index string) *GeoDB {
 	}
 }
 
+// Adds a point of interest into the geo spatial database, given
+// a key id and a map containing the longtitude and latitude
+// coordinates. A generic error is returned for all errors.
 func (db *GeoDB) Insert(keyId string, coord map[string]float64) error {
 	conn := db.pool.Get()
 	defer conn.Close()
@@ -31,6 +37,9 @@ func (db *GeoDB) Insert(keyId string, coord map[string]float64) error {
 	return nil
 }
 
+// Returns a map containing the longitude and latitude coordinates
+// of a point of interest given its key id. A generic error is
+// returned for all errors.
 func (db *GeoDB) Select(keyId string) (map[string]float64, error) {
 	conn := db.pool.Get()
 	defer conn.Close()
@@ -50,6 +59,10 @@ func (db *GeoDB) Select(keyId string) (map[string]float64, error) {
 	return coord, nil
 }
 
+// Returns the key id of the closest point to a point of interest
+// (lon, lat) a radius to search within. Returns a specific error
+// if no nearby point is found within the given radius. A generic
+// error is returned for all other errors.
 func (db *GeoDB) SelectNearestInRadius(coords map[string]float64, radius float64) (string, error) {
 	conn := db.pool.Get()
 	defer conn.Close()
@@ -75,6 +88,10 @@ func (db *GeoDB) SelectNearestInRadius(coords map[string]float64, radius float64
 	return closestPOIKeyId, nil
 }
 
+// Deletes a specific point of interest (lon, lat) by key in the
+// current geospacial index. Returns a specific error if the passed
+// in key is no found. A generic error is returned for all other
+// errors.
 func (db *GeoDB) Delete(keyId string) error {
 	conn := db.pool.Get()
 	defer conn.Close()
@@ -90,6 +107,8 @@ func (db *GeoDB) Delete(keyId string) error {
 	return nil
 }
 
+// Clears the entire key value store. A generic error is returned for
+// all errors.
 func (db *GeoDB) Clear() error {
 	conn := db.pool.Get()
 	defer conn.Close()
